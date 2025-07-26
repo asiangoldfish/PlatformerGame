c883b931c5d1a26a37eb3f162c34ac4ba3797f14
Khai Duong <duongkhai@gmail.com>
2025-07-26 22:59:44 +0200

    [BUG] Fix missing FW namespace in PhysicsDemo

--- framework/ECS/BaseScene.h
+++ framework/ECS/BaseScene.h
@@ -5,26 +5,27 @@
 #include "Viewport.h"
 
 namespace FW {
-/** A scene is a game state. It is the overall supervisor of all things that
- * happens thereafter. Only one scene can be active at any time.
- * 
- * A scene can only have one direct child node. To build out the scene, users
- * must add child nodes to the scene root node.
- */
-class BaseScene {
-public:
-    BaseScene() = default;
-    virtual ~BaseScene();
-    virtual void init();
-    virtual void cleanUp() = 0;
-    virtual void update(float delta);
+    /** A scene is a game state. It is the overall supervisor of all things that
+     * happens thereafter. Only one scene can be active at any time.
+     *
+     * A scene can only have one direct child node. To build out the scene,
+     * users must add child nodes to the scene root node.
+     */
+    class BaseScene {
+    public:
+        BaseScene() = default;
+        virtual ~BaseScene();
+        virtual void init();
+        virtual void cleanUp() = 0;
+        virtual void update(float delta);
 
-    ref<Entity> getRoot() { return root; }
-    ref<Viewport> getViewport() { return viewport; }
-protected:
-    ref<Entity> root;
+        ref<Entity> getRoot() { return root; }
+        ref<Viewport> getViewport() { return viewport; }
 
-private:
-    ref<Viewport> viewport;
-};
+    protected:
+        ref<Entity> root;
+
+    private:
+        ref<Viewport> viewport;
+    };
 } // namespace FW
