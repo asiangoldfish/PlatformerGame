ce66a9c1feca7088b777b7fb15718c15e076cdf0
Khai Duong <duongkhai@gmail.com>
2025-08-31 18:13:01 +0200

    [ENH] New class: ShaderManager
    Many components of the Engine and the clients rely on reusing the same
    shader resources. Instead of passing shaders across the project or
    storing multiple references to a single shader object on the GPU, the
    ShaderManager owns the C++ Shader objects and lets users fetch or
    interact with them via its APIs.

---
+++ Framework/ResourceManagement/ShaderManager.cpp
@@ -0,0 +1,24 @@
+#include "ShaderManager.h"
+#include "Log.h"
+
+namespace FW {
+    void ShaderManager::createShaderFromFiles(const std::string& name,
+                                              const std::string& vertexSrc,
+                                              const std::string& fragmentSrc) {
+
+        auto iter = shaders.find(name);
+        if (iter == shaders.end()) {
+            shaders.try_emplace(name, Shader{ vertexSrc, fragmentSrc });
+        }
+    }
+
+    Shader* ShaderManager::bind(const std::string& name) {
+        auto iter = shaders.find(name);
+        if (iter != shaders.end()) {
+            iter->second.bind();
+            return &iter->second;
+        } else {
+            return nullptr;
+        }
+    }
+}
---
+++ Framework/ResourceManagement/ShaderManager.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <Shader.h>
+
+#include <unordered_map>
+
+namespace FW {
+    /**
+     * Credits to Scott Meyer for the Singleton pattern implementation.
+     * https://laristra.github.io/flecsi/src/developer-guide/patterns/meyers_singleton.html
+     */
+    class ShaderManager {
+    public:
+        static ShaderManager& get() {
+            static ShaderManager s;
+            return s;
+        }
+
+        ShaderManager(const ShaderManager&) = delete;
+        ShaderManager& operator=(const ShaderManager&) = delete;
+
+    private:
+        ShaderManager() {}
+        ~ShaderManager() {}
+
+    public:
+        void createShaderFromFiles(const std::string& name,
+                                   const std::string& vertexSrc,
+                                   const std::string& fragmentSrc);
+
+        /** If the shader cannot be found, then return false. */
+        Shader* bind(const std::string& name);
+
+    private:
+        /** TODO find some way to avoid heap allocation */
+        std::unordered_map<std::string, Shader> shaders;
+    };
+}
