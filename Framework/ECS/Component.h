252694cd051ae9a905bb3d8847e285119cc8f197
Khai Duong <duongkhai@gmail.com>
2025-09-01 19:03:08 +0200

    fix: Fix error where Entities don't appear on screen
    The position was never initialised on creation.

--- Framework/ECS/Component.h
+++ Framework/ECS/Component.h
@@ -92,7 +92,7 @@ namespace FW {
         // Transformation
         glm::mat4 modelMatrix;
         std::string shader;
-        glm::vec3 position;
+        glm::vec3 position{0.0f};
         float yaw = 0.0f, pitch = 0.0f, roll = 0.0f;
         glm::vec3 scale = glm::vec3{ 1.0f };
         glm::vec3 rotation = glm::vec3{ 0.0f };
